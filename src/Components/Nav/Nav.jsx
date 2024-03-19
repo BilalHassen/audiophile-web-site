0bb8d4de4a807756010e07f2e6ef1567a337c77e
BilalHassen <bilalhassen097@gmail.com>
2024-03-19 16:22:17 -0400

    change text weight in nav boxes

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -14,7 +14,7 @@ export default function Nav() {
         <ul className="nav__list-container">
           <li className="nav__list-item">
             <img className="nav__list-img" src={headphones}></img>
-            <p className="nav__list-title">Earphones</p>
+            <p className="nav__list-title">headphones</p>
             <div className="nav__shop-container">
               <p className="nav__text">shop</p>
               <img className="nav__orange-icon" src={orangeIcon}></img>
@@ -22,7 +22,7 @@ export default function Nav() {
           </li>
           <li className="nav__list-item">
             <img className="nav__list-img" src={speakers}></img>
-            <p className="nav__list-title">Earphones</p>
+            <p className="nav__list-title">speakers</p>
             <div className="nav__shop-container">
               <p className="nav__text">shop</p>
               <img className="nav__orange-icon" src={orangeIcon}></img>
