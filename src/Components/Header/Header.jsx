029ea6a39199c638e3fc02ce71fca2dffb561d2c
BilalHassen <bilalhassen097@gmail.com>
2024-03-19 08:19:24 -0400

    styled basic nav menu for mobile

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,47 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import headphones from "../../assets/shared/desktop/thumbnail-headphones.png";
+import speakers from "../../assets/shared/desktop/image-category-thumbnail-speakers.png";
+import earphones from "../../assets/shared/desktop/image-category-thumbnail-earphones.png";
+import orangeIcon from "../../assets/shared/desktop/icon-arrow-right.svg";
+import "./Header.scss";
+
 export default function Header() {
   return (
     <>
-      <h1>this is the header component</h1>
+      <div className="header">
+        AUDIOPHILE
+        <FontAwesomeIcon icon={faBars} className="header__icon" />
+      </div>
+
+      <nav className="nav">
+        <ul className="nav__list-container">
+          <li className="nav__list-item">
+            <img className="nav__list-img" src={headphones}></img>
+            <p className="nav__list-title">Earphones</p>
+            <div className="nav__shop-container">
+              <p className="nav__text">shop</p>
+              <img className="nav__orange-icon" src={orangeIcon}></img>
+            </div>
+          </li>
+          <li className="nav__list-item">
+            <img className="nav__list-img" src={speakers}></img>
+            <p className="nav__list-title">Earphones</p>
+            <div className="nav__shop-container">
+              <p className="nav__text">shop</p>
+              <img className="nav__orange-icon" src={orangeIcon}></img>
+            </div>
+          </li>
+          <li className="nav__list-item">
+            <img className="nav__list-img" src={earphones}></img>
+            <p className="nav__list-title">Earphones</p>
+            <div className="nav__shop-container">
+              <p className="nav__text">shop</p>
+              <img className="nav__orange-icon" src={orangeIcon}></img>
+            </div>
+          </li>
+        </ul>
+      </nav>
     </>
   );
 }
