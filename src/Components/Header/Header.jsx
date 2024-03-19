eed23afb6aaa8c99f868af7a9e9b82c9882f5d70
BilalHassen <bilalhassen097@gmail.com>
2024-03-19 15:10:54 -0400

    add dynamic styling for header on tablet screen sizing

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,26 +2,55 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import cartIcon from "../../assets/shared/desktop/icon-cart.svg";
 import Nav from "../../Components/Nav/Nav";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Header.scss";
 export default function Header() {
   const [isActive, setIsActive] = useState(false);
+  const [isTablet, setTablet] = useState(false);
+
+  const handleTabletSize = () => {
+    if (window.innerWidth >= 768) {
+      setTablet(true);
+    } else if (window.innerWidth < 768 || window.innerWidth > 1280) {
+      setTablet(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth >= 768) {
+      window.addEventListener("resize", handleTabletSize);
+    } else if (window.innerWidth < 768 || window.innerWidth >= 1280) {
+      window.removeEventListener("resize", handleTabletSize);
+    }
+  }, [window.innerWidth]);
 
   const handleNavDisplay = () => {
     setIsActive(!isActive);
   };
 
-  console.log(isActive);
-
   return (
     <>
       <section className="header">
-        <FontAwesomeIcon
-          icon={faBars}
-          className="header__icon"
-          onClick={handleNavDisplay}
-        />
-        <h1 className="header__title">audiophile</h1>
+        {isTablet ? (
+          <div className="header__tablet-wrapper">
+            <FontAwesomeIcon
+              icon={faBars}
+              className="header__icon"
+              onClick={handleNavDisplay}
+            />
+            <h1 className="header__title">audiophile</h1>
+          </div>
+        ) : (
+          <>
+            <FontAwesomeIcon
+              icon={faBars}
+              className="header__icon"
+              onClick={handleNavDisplay}
+            />
+            <h1 className="header__title">audiophile</h1>
+          </>
+        )}
+
         <img classname="header__cart-icon" src={cartIcon} alt="cart-icon"></img>
       </section>
       {isActive ? <Nav /> : ""}
