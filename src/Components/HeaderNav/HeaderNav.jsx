2ae45a68045cfc38afa1cad01b5a9ac2dde3ac34
BilalHassen <bilalhassen097@gmail.com>
2024-03-20 14:26:36 -0400

    add hover effect for nav items

--- src/Components/HeaderNav/HeaderNav.jsx
+++ src/Components/HeaderNav/HeaderNav.jsx
@@ -4,7 +4,7 @@ export default function HeaderNav() {
   return (
     <section className="header__nav">
       <ul className="header__nav-wrapper">
-        <li className="header__nav-item one">home</li>
+        <li className="header__nav-item">home</li>
         <li className="header__nav-item">headphones</li>
         <li className="header__nav-item">speakers</li>
         <li className="header__nav-item">earphones</li>
