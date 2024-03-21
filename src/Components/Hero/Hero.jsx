cfb82daabf5a0742134dfc05b79a0ba5b400fc43
BilalHassen <bilalhassen097@gmail.com>
2024-03-21 14:49:06 -0400

    create component for hero and add basic html structure

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,18 @@
+export default function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero__product-box">
+        <p className="hero__product-text">new product</p>
+      </div>
+      <div className="hero__title">
+        <h1 className="hero__title-text">xx99 mark II headphones</h1>
+      </div>
+      <div className="hero__product-description">
+        <p className="hero__description-text">
+          Experience natrual, lifelike audio and exceptional build quality maked
+          for the passionate music enthusiast
+        </p>
+      </div>
+    </section>
+  );
+}
