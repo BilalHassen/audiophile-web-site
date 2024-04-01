830aad9d5d3f1cd1f4f6ce088c1794622e12b887
BilalHassen <bilalhassen097@gmail.com>
2024-04-01 14:03:19 -0400

    add text to audiopholeDescription component

--- src/Components/AudiophileDescription/AudiophileDescription.jsx
+++ src/Components/AudiophileDescription/AudiophileDescription.jsx
@@ -3,7 +3,19 @@ import "./AudiophileSescription.scss";
 export default function AudiophileSescription() {
   return (
     <>
-      <h1> this is the description component</h1>
+      <section className="audiophileDescription">
+        <h1 className="audiophileDescription__title">
+          bringing you the <span>best</span> audio gear
+        </h1>
+        <p className="audiophileDescription__para">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </section>
     </>
   );
 }
