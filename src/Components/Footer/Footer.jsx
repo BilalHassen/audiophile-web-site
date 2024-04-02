d574a7f04eee6807a747f1bf82c42b2d56c8b947
BilalHassen <bilalhassen097@gmail.com>
2024-04-02 11:50:01 -0400

    add footer icons and copyright text

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -23,6 +23,22 @@ export default function Footer() {
             to helping you get the most out of personal audio. Come and visit
             our demo facility - we’re open 7 days a week.
           </p>
+          <div className="footer__copyright">
+            <p className="footer__copyright-text">
+              Copyright 2021. All Rights Reserved
+            </p>
+            <ul className="footer__icons-container">
+              <li className="footer__icons">
+                <img className="footer__icon" src={facebookIcon}></img>
+              </li>
+              <li className="footer__icons">
+                <img className="footer__icon" src={instagramIcon}></img>
+              </li>
+              <li className="footer__icons">
+                <img className="footer__icon" src={twitterIcon}></img>
+              </li>
+            </ul>
+          </div>
         </div>
       </footer>
     </>
