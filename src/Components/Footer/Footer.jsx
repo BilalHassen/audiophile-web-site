8fc643fe6792fd91d6fa3ab67e0c3a8edc85c314
BilalHassen <bilalhassen097@gmail.com>
2024-04-02 11:48:54 -0400

    add footer and nav structure with images

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import "./Footer.scss";
-
+import logo from "../../assets/shared/desktop/logo.svg";
+import facebookIcon from "../../assets/shared/desktop/icon-facebook.svg";
+import instagramIcon from "../../assets/shared/desktop/icon-instagram.svg";
+import twitterIcon from "../../assets/shared/desktop/icon-twitter.svg";
 export default function Footer() {
   return (
     <>
-      <h1>this is the footer component</h1>
+      <footer className="footer">
+        <div className="footer__logo-container">
+          <img className="footer__logo" src={logo} alt={"footer logo"}></img>
+          <ul className="footer__nav">
+            <li className="footer__nav-item">home</li>
+            <li className="footer__nav-item">headphones</li>
+            <li className="footer__nav-item">speaker</li>
+            <li className="footer__nav-item">earphones</li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
