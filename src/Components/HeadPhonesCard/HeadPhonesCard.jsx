548a5b9ea0f8c66b84e7c28250fb089ffbb03f56
BilalHassen <bilalhassen097@gmail.com>
2024-04-13 16:22:43 -0400

    get data from api and create headphones card for each item

--- src/Components/HeadPhonesCard/HeadPhonesCard.jsx
+++ src/Components/HeadPhonesCard/HeadPhonesCard.jsx
@@ -1,1 +1,27 @@
+import React from "react";
 import "./HeadPhonesCard.scss";
+
+export default function HeadPhonesCard({
+  id,
+  name,
+  description,
+  urlMobile,
+  urlTablet,
+  urlDesktop,
+  console,
+}) {
+  return (
+    <div className="headphones__card">
+      <div className="headphones__image-container">
+        <img
+          className="headphones__img"
+          src={urlMobile}
+          alt="headphones image"
+        ></img>
+      </div>
+      <h1 className="headphones__card-title">{name}</h1>
+      <p className="headphones__para">{description}</p>
+      <button className="headphones__button">View Details</button>
+    </div>
+  );
+}
--- src/Pages/HeadPhones-Page/HeadPhonesPage.jsx
+++ src/Pages/HeadPhones-Page/HeadPhonesPage.jsx
@@ -1,10 +1,39 @@
 import Header from "../../Components/Header/Header";
 import AudiophileDescription from "../../Components/AudiophileDescription/AudiophileDescription";
 import Footer from "../../Components/Footer/Footer";
+import HeadPhonesCard from "../../Components/HeadPhonesCard/HeadPhonesCard";
+import axios from "axios";
+import { useEffect, useState } from "react";
 export default function HeadPhonesPage() {
+  const [headphonesData, setHeadphonesData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await axios.get("http://localhost:8080/products");
+      console.log(response.data);
+      setHeadphonesData(response.data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log({ headphonesData });
+
   return (
     <>
       <Header />
+      <section className="headphones">
+        {headphonesData.map((data) => (
+          <HeadPhonesCard
+            id={data.id}
+            name={data.name}
+            description={data.description}
+            urlMobile={data.url_mobile}
+            urlTablet={data.url_tablet}
+            urlDesktop={data.url_desktop}
+          />
+        ))}
+      </section>
       <AudiophileDescription />
       <Footer />
     </>
