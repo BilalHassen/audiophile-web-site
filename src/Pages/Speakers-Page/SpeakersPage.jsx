c7e81083c147c2c22d8c14aacc56b1273e773896
BilalHassen <bilalhassen097@gmail.com>
2024-05-02 10:46:23 -0400

    SpeakersPage complete without speakers item cards

--- src/Pages/Speakers-Page/SpeakersPage.jsx
+++ src/Pages/Speakers-Page/SpeakersPage.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import Header from "../../Components/Header/Header";
 import AudiophileDescription from "../../Components/AudiophileDescription/AudiophileDescription";
+import Footer from "../../Components/Footer/Footer";
 export default function SpeakersPage() {
   return (
     <>
-      <h1>Speakers Page...</h1>
+      <Header />
+      <section className="speakers">
+        <h1 className="speakers__title">speakers</h1>
+      </section>
+      <AudiophileDescription />
+      <Footer />
     </>
   );
 }
