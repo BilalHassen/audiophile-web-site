2b1b711b6a5d698bb98d269efd1fc387918fc092
BilalHassen <bilalhassen097@gmail.com>
2024-05-02 11:23:30 -0400

    assign api url to variable

--- src/Pages/Speakers-Page/SpeakersPage.jsx
+++ src/Pages/Speakers-Page/SpeakersPage.jsx
@@ -2,7 +2,26 @@ import axios from "axios";
 import Header from "../../Components/Header/Header";
 import AudiophileDescription from "../../Components/AudiophileDescription/AudiophileDescription";
 import Footer from "../../Components/Footer/Footer";
+import { useEffect, useState } from "react";
 export default function SpeakersPage() {
+  const [speakersData, setSpeakersData] = useState([]);
+
+  const apiURL = "http://localhost:8080/products/speakers";
+
+  useEffect(() => {
+    const getSpeakersData = async () => {
+      try {
+        let response = await axios.get(apiURL);
+        let speakersData = response.data;
+        setSpeakersData(speakersData);
+      } catch (error) {
+        console.log("Error fetching data:", error);
+      }
+    };
+
+    getSpeakersData();
+  }, []);
+
   return (
     <>
       <Header />
