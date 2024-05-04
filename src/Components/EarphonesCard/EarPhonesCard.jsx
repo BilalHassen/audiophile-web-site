41a4c9f2bb2c50eea742c8ab9f9fdb224583f66e
BilalHassen <bilalhassen097@gmail.com>
2024-05-04 16:25:00 -0400

    add jsx structure to the EarPhones-card

--- src/Components/EarphonesCard/EarPhonesCard.jsx
+++ src/Components/EarphonesCard/EarPhonesCard.jsx
@@ -5,7 +5,23 @@ import { useEffect, useState } from "react";
 export default function EarPhonesCard() {
   return (
     <>
-      <h1>headphones card</h1>
+      <div className={`earphones__card`}>
+        <div className="earphones__image-container">
+          <img
+            className="earphones__img"
+            src={urlMobile}
+            alt="earphones image"
+          ></img>
+        </div>
+        <div className="earphones__text-container">
+          <h2 className="earphones__new-product"></h2>
+          <h1 className={`earphones__card-title`}>{}</h1>
+          <p className="earphones__para">{}</p>
+          <div className="earphones__button-container">
+            <button className="earphones__button">see product</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
