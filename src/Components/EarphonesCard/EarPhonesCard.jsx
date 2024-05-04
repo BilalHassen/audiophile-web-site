ef2cfd89b25be19a391f6f18e7750d6112be2809
BilalHassen <bilalhassen097@gmail.com>
2024-05-04 17:07:59 -0400

    add logic for adding correct image urls based on screen sizes

--- src/Components/EarphonesCard/EarPhonesCard.jsx
+++ src/Components/EarphonesCard/EarPhonesCard.jsx
@@ -9,15 +9,55 @@ export default function EarPhonesCard({
   urlTablet,
   urlDesktop,
 }) {
+  const [isMobile, setMobile] = useState(false);
+  const [isTablet, setTablet] = useState(false);
+  const [isDesktop, setDesktop] = useState(false);
+
+  const handleEarPhonesSize = () => {
+    let currentWidth = window.innerWidth;
+    setMobile(currentWidth < 768);
+    setTablet(currentWidth >= 768 && currentWidth < 1366);
+    setDesktop(currentWidth >= 1366);
+  };
+
+  useEffect(() => {
+    handleEarPhonesSize();
+
+    window.addEventListener("resize", handleEarPhonesSize);
+
+    const cleanUpFunction = () => {
+      return window.removeEventListener("resize", handleEarPhonesSize);
+    };
+
+    // function will be executed when component unmounts
+    return cleanUpFunction;
+  }, []);
+
   return (
     <>
       <div className={`earphones__card`}>
         <div className="earphones__image-container">
-          <img
-            className="earphones__img"
-            src={urlMobile}
-            alt="earphones image"
-          ></img>
+          {isMobile ? (
+            <img
+              className="earphones__img"
+              src={urlMobile}
+              alt="earphones image"
+            ></img>
+          ) : null}
+          {isTablet ? (
+            <img
+              className="earphones__img"
+              src={urlTablet}
+              alt="earphones image"
+            ></img>
+          ) : null}
+          {isDesktop ? (
+            <img
+              className="earphones__img"
+              src={urlDesktop}
+              alt="earphones image"
+            ></img>
+          ) : null}
         </div>
         <div className="earphones__text-container">
           <h2 className="earphones__new-product">new product</h2>
