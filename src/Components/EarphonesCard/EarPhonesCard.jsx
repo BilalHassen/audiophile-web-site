0cdbb83972a951c963198851bbb03c6a21c18715
BilalHassen <bilalhassen097@gmail.com>
2024-05-04 16:22:25 -0400

    created EarPhonesCard and import items in function and stylesheet

---
+++ src/Components/EarphonesCard/EarPhonesCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HeadPhonesCard.scss";
+import { useEffect, useState } from "react";
+
+export default function EarPhonesCard() {
+  return (
+    <>
+      <h1>headphones card</h1>
+    </>
+  );
+}
