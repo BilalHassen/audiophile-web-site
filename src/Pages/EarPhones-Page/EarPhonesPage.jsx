ef84be1ca38373d9d594a484715aea52c5b4259b
BilalHassen <bilalhassen097@gmail.com>
2024-05-04 15:13:21 -0400

    import necessary items to the EarPhones-page and stylesheet

--- src/Pages/EarPhones-Page/EarPhonesPage.jsx
+++ src/Pages/EarPhones-Page/EarPhonesPage.jsx
@@ -1,5 +1,9 @@
+import Header from "../../Components/Header/Header";
+import AudiophileDescription from "../../Components/AudiophileDescription/AudiophileDescription";
+import Footer from "../../Components/Footer/Footer";
+import HeadPhonesCard from "../../Components/HeadPhonesCard/HeadPhonesCard";
 import axios from "axios";
-
+import { useEffect, useState } from "react";
 export default function EarPhonesPage() {
   return (
     <>
