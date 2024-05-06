3177e1a778518653d473d4efc99b4a63120219b8
BilalHassen <bilalhassen097@gmail.com>
2024-05-06 12:02:35 -0400

    create default ProductPage component and create stylesheet

---
+++ src/Pages/ProductPage/ProductPage.jsx
@@ -0,0 +1,10 @@
+import "../../Components/Header/Header";
+import "./ProductsPage";
+
+export default function ProductPage() {
+  return (
+    <>
+      <h1>Dyanmic Products Page </h1>
+    </>
+  );
+}
