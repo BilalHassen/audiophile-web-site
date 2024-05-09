ce7a065908ddba8560d8d63d8380daeb4f9ae9db
BilalHassen <bilalhassen097@gmail.com>
2024-05-09 12:14:22 -0400

    add basic structure to ProductCard component

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -22,5 +22,32 @@ export default function ProductCard({
     isNew();
   }, []);
 
-  return <></>;
+  return (
+    <>
+      <div className="product__card">
+        <img className="product__img" src={urlMobile}></img>
+        {is_new ? (
+          <h2 className={is_new ? "product__new-product" : null}>
+            {" "}
+            new product
+          </h2>
+        ) : null}
+        <h1 className="product__title">{name}</h1>
+        <p className="product__description">{description}</p>
+        <p className="product__price">$ {price}</p>
+        <div className="product__add-delete">
+          <div className="product__controller-container">
+            <button className="product__delete">-</button>
+            <p className="product__number">1</p>
+            <button className="product__delete">+</button>
+          </div>
+          <button className="product__button">add to cart</button>
+        </div>
+
+        <div className="product__features-container">
+          <p className="product__features">{}</p>
+        </div>
+      </div>
+    </>
+  );
 }
