1b79fa1802e705fe580690a1c0a2b9a027acac59
BilalHassen <bilalhassen097@gmail.com>
2024-06-02 11:44:16 -0400

    get cart items and calculate the prices, add cart component with needed information

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,9 +2,11 @@ import { useParams } from "react-router-dom";
 import "./Cart.scss";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import CartItems from "../CartItems/CartItems";
 
 export default function Cart() {
-  const [cardData, setCardData] = useState([]);
+  const [cartData, setCartData] = useState([]);
+  const [total, setTotal] = useState(0);
   const card_id = localStorage.getItem("cart_id");
 
   useEffect(() => {
@@ -13,23 +15,45 @@ export default function Cart() {
         `http://localhost:8080/cart/getitems/${card_id}`
       );
       let products_data = response.data;
-      setCardData(products_data);
+      setCartData(products_data);
     };
     getCartData();
   }, []);
 
-  console.log("dynamic data:", cardData);
+  console.log("dynamic data:", cartData);
+
+  useEffect(() => {
+    let totalAmount = 0;
+
+    for (let i = 0; i < cartData.length; i++) {
+      totalAmount = totalAmount += cartData[i].price;
+    }
+
+    setTotal(totalAmount);
+  }, [cartData]);
+
+  console.log(total);
 
   return (
     <div className="cart">
       <div className="cart__container">
         <div className="cart__text-container">
-          <h3 className="cart__count">{}</h3>
+          <h3 className="cart__count">{cartData.length}</h3>
           <button className="cart__remove-button">Remove all</button>
         </div>
+        {cartData.map((items) => (
+          <CartItems
+            item_name={items.item_name}
+            quantity={items.quantity}
+            price={items.price}
+            url_mobile={items.url_mobile}
+            url_tablet={items.url_tablet}
+            url_desktop={items.url_desktop}
+          />
+        ))}
         <div className="card__text-container-2">
           <p className="card__total">total</p>
-          <p className="card__total-amount">{}</p>
+          <p className="card__total-amount">${total}</p>
         </div>
       </div>
     </div>
---
+++ src/Components/CartItems/CartItems.jsx
@@ -0,0 +1,16 @@
+import "./CartItems.scss";
+
+export default function CartItems({
+  item_name,
+  quantity,
+  price,
+  url_mobile,
+  url_tablet,
+  url_desktop,
+}) {
+  return (
+    <>
+      <h1>cart items component</h1>
+    </>
+  );
+}
