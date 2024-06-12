322794934c166f1a24f55eb29792dc9261c86ca3
BilalHassen <bilalhassen097@gmail.com>
2024-06-12 10:29:35 -0400

    add summary items component

--- src/Components/Summary/Summary.jsx
+++ src/Components/Summary/Summary.jsx
@@ -1,8 +1,53 @@
+import { useState, useEffect } from "react";
+
 export default function Summary({ cartData }) {
   console.log(cartData);
+
+  const [screenSize, setScreenSize] = useState(handleImageUrl());
+  function handleImageUrl() {
+    let width = window.innerWidth;
+
+    if (width < 768) {
+      return "mobile";
+    }
+
+    if (width >= 768 && width < 1366) {
+      return "tablet";
+    }
+
+    if (width >= 1366) {
+      return "desktop";
+    }
+  }
+
+  // update the screensize state with the returned value from handleImageUrl
+  const handleScreenSize = () => {
+    // update screenSize variable with value returned from handleImageUrl
+    setScreenSize(handleImageUrl());
+  };
+
+  useEffect(() => {
+    // update screen size when component mounts
+    handleScreenSize();
+
+    const resize = () => {
+      handleScreenSize();
+    };
+
+    // add event listener to window
+    window.addEventListener("resize", resize);
+
+    // clean up function
+    const removeEventListener = () => {
+      return window.removeEventListener("resize", resize);
+    };
+
+    return removeEventListener;
+  }, []);
+
   return (
     <>
-      <h1>summary of the order</h1>
+      <div className="summary"></div>
     </>
   );
 }
