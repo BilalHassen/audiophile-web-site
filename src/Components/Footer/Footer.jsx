967ab5fd08836ae04e73da268d3b6e835a96a02b
BilalHassen <bilalhassen097@gmail.com>
2024-07-02 17:48:52 -0400

    fix responsive styling on footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,38 +7,40 @@ export default function Footer() {
   return (
     <>
       <footer className="footer">
-        <hr className="footer__divider"></hr>
-        <div className="footer__logo-container">
-          <img className="footer__logo" src={logo} alt={"footer logo"}></img>
-          <ul className="footer__nav">
-            <li className="footer__nav-item">home</li>
-            <li className="footer__nav-item">headphones</li>
-            <li className="footer__nav-item">speaker</li>
-            <li className="footer__nav-item">earphones</li>
-          </ul>
-        </div>
-        <div className="footer__info-txt">
-          <p className="footer__para">
-            Audiophile is an all in one stop to fulfill your audio needs. We're
-            a small team of music lovers and sound specialists who are devoted
-            to helping you get the most out of personal audio. Come and visit
-            our demo facility - we’re open 7 days a week.
-          </p>
-          <div className="footer__copyright">
-            <p className="footer__copyright-text">
-              Copyright 2021. All Rights Reserved
-            </p>
-            <ul className="footer__icons-container">
-              <li className="footer__icons">
-                <img className="footer__icon" src={facebookIcon}></img>
-              </li>
-              <li className="footer__icons">
-                <img className="footer__icon-twitter" src={twitterIcon}></img>
-              </li>
-              <li className="footer__icons">
-                <img className="footer__icon" src={instagramIcon}></img>
-              </li>
+        <div className="footer__wrapper">
+          <hr className="footer__divider"></hr>
+          <div className="footer__logo-container">
+            <img className="footer__logo" src={logo} alt={"footer logo"}></img>
+            <ul className="footer__nav">
+              <li className="footer__nav-item">home</li>
+              <li className="footer__nav-item">headphones</li>
+              <li className="footer__nav-item">speaker</li>
+              <li className="footer__nav-item">earphones</li>
             </ul>
+          </div>
+          <div className="footer__info-txt">
+            <p className="footer__para">
+              Audiophile is an all in one stop to fulfill your audio needs.
+              We're a small team of music lovers and sound specialists who are
+              devoted to helping you get the most out of personal audio. Come
+              and visit our demo facility - we’re open 7 days a week.
+            </p>
+            <div className="footer__copyright">
+              <p className="footer__copyright-text">
+                Copyright 2021. All Rights Reserved
+              </p>
+              <ul className="footer__icons-container">
+                <li className="footer__icons">
+                  <img className="footer__icon" src={facebookIcon}></img>
+                </li>
+                <li className="footer__icons">
+                  <img className="footer__icon-twitter" src={twitterIcon}></img>
+                </li>
+                <li className="footer__icons">
+                  <img className="footer__icon" src={instagramIcon}></img>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </footer>
