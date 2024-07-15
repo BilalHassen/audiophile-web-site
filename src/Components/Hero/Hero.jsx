21f4d476604c1b51dc0c17b2865c9b673c7fabb4
BilalHassen <bilalhassen097@gmail.com>
2024-07-15 15:15:47 -0400

    fix typo in header text

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -18,7 +18,7 @@ export default function Hero() {
             <div className="hero__product-description">
               <p className="hero__description-text">
                 Experience natrual, lifelike audio and exceptional build quality
-                maked for the passionate music enthusiast
+                made for the passionate music enthusiast
               </p>
             </div>
             <Link to="/products/2">
